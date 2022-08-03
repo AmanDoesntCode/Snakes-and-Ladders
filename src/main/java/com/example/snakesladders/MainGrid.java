22f4ae5ccdba6619258a6a3e725be22b7a7eb3ec
Aman Singh <98482324+AmanDoesntCode@users.noreply.github.com>
2022-08-04 04:12:50 +0530

    completed

--- src/main/java/com/example/snakesladders/MainGrid.java
+++ src/main/java/com/example/snakesladders/MainGrid.java
@@ -11,12 +11,10 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane; //stack is a tool used to put stuff on top of each other
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
 import java.io.IOException;
 import java.util.Random;
 
@@ -48,8 +46,6 @@ public class MainGrid extends Application {
 
     public int poscirc1 = 1;
     public int poscirc2 = 1;
-
-
 
 
     private Group subGridGroup = new Group();
@@ -93,8 +89,25 @@ public class MainGrid extends Application {
                         randno.setText(String.valueOf(rand));
                         moveplayer1();
                         playermove(p1posX,p1posY,p1);
+                        p1pos += rand;
                         p1turn = false;
                         p2turn = true;
+
+                        //ladder
+                        if (p1posX == 175 && p1posY ==  665 ){playermove(p1posX = 105,p1posY = 455,p1);poscirc1=0;}
+                        if (p1posX == 665 && p1posY == 665 ){playermove(p1posX = 595,p1posY = 595,p1);poscirc1=0;}
+                        if (p1posX == 455 && p1posY ==  525 ){playermove(p1posX = 525,p1posY = 315,p1);poscirc1=0;}
+                        if (p1posX == 315 && p1posY ==  315 ){playermove(p1posX = 245,p1posY = 105,p1);poscirc1=1;}
+                        if (p1posX == 35 && p1posY ==  245 ){playermove(p1posX = 105,p1posY = 35,p1);poscirc1=0;}
+                        if (p1posX == 595 && p1posY ==  175 ){playermove(p1posX = 665,p1posY = 105,p1);poscirc1=1;}
+
+                        //snakes
+                        if (p1posX == 315 && p1posY ==  595 ){playermove(p1posX = 525,p1posY = 595,p1);poscirc1=0;}
+                        if (p1posX == 665 && p1posY == 455 ){playermove(p1posX = 245,p1posY = 665,p1);poscirc1=1;}
+                        if (p1posX == 455 && p1posY ==  385 ){playermove(p1posX = 315,p1posY = 525,p1);poscirc1=1;}
+                        if (p1posX == 175 && p1posY ==  245 ){playermove(p1posX = 35,p1posY = 315,p1);poscirc1=0;}
+                        if (p1posX == 385 && p1posY ==  245 ){playermove(p1posX = 595,p1posY = 315,p1);poscirc1=0;}
+                        if (p1posX == 245 && p1posY ==  35 ){playermove(p1posX = 385,p1posY = 175,p1);poscirc1=0;}
 
 
                     }
@@ -102,7 +115,7 @@ public class MainGrid extends Application {
             }
         });
 
-        Button button2 = new Button("raghav");
+        Button button2 = new Button("Raghav");
         button2.setTranslateX(600);
         button2.setTranslateY(720);
         button2.setOnAction(new EventHandler<ActionEvent>() {
@@ -114,8 +127,25 @@ public class MainGrid extends Application {
                         randno.setText(String.valueOf(rand));
                         moveplayer2();
                         playermove(p2posX,p2posY,p2);
+                        p2pos += rand;
                         p2turn = false;
                         p1turn = true;
+
+//                        //ladders
+                        if (p2posX == 175 && p2posY ==  665 ){playermove(p2posX = 105,p2posY = 455,p2);poscirc2=0;}
+                        if (p2posX == 665 && p2posY == 665 ){playermove(p2posX = 595,p2posY = 595,p2);poscirc2=0;}
+                        if (p2posX == 455 && p2posY ==  525 ){playermove(p2posX = 525,p2posY = 315,p2);poscirc2=0;}
+                        if (p2posX == 315 && p2posY ==  315 ){playermove(p2posX = 245,p2posY = 105,p2);poscirc2=1;}
+                        if (p2posX == 35 && p2posY ==  245 ){playermove(p2posX = 205,p2posY = 35,p2);poscirc2=0;}
+                        if (p2posX == 595 && p2posY ==  175 ){playermove(p2posX = 665,p2posY = 105,p2);poscirc2=1;}
+
+                        //snakes
+                        if (p2posX == 315 && p2posY ==  595 ){playermove(p2posX = 525,p2posY = 595,p2);poscirc1=0;}
+                        if (p2posX == 665 && p2posY == 455 ){playermove(p2posX = 245,p2posY = 665,p2);poscirc2=1;}
+                        if (p2posX == 455 && p2posY ==  385 ){playermove(p2posX = 315,p2posY = 525,p2);poscirc2=1;}
+                        if (p2posX == 175 && p2posY ==  245 ){playermove(p2posX = 35,p2posY = 315,p2);poscirc2=0;}
+                        if (p2posX == 385 && p2posY ==  245 ){playermove(p2posX = 595,p2posY = 315,p2);poscirc2=0;}
+                        if (p2posX == 245 && p2posY ==  35 ){playermove(p2posX = 385,p2posY = 175,p2);poscirc2=0;}
 
                     }
                 }
@@ -145,10 +175,10 @@ public class MainGrid extends Application {
             }
         });
         randno = new Label("0");
-        randno.setTranslateX(300);
-        randno.setTranslateY(725);
+        randno.setTranslateX(345);
+        randno.setTranslateY(745);
 
-        Image imgbg = new Image("D:\\Java\\SnakesLadders\\src\\main\\resources\\icons\\Pink Snakes - Small.jpg");
+        Image imgbg = new Image("D:\\Java\\SnakesLadders\\src\\main\\resources\\icons\\SNAKES AND LADDERS.png");
         ImageView bgphoto = new ImageView();
         bgphoto.setImage(imgbg);
         bgphoto.setFitHeight(700);
@@ -164,7 +194,7 @@ public class MainGrid extends Application {
             if(poscirc1%2 == 0)p1posX -= 70;
             if(p1posX>665){ p1posY-=70; p1posX-=70; poscirc1 ++; }
             if(p1posX<35){ p1posY-=70; p1posX+=70; poscirc1 ++; }
-            if(p1posX<35 || p1posY<35){ p1posY=35; p1posX=35; randno.setText("aman wonnn !!"); gB.setText("Start Again !"); }
+            if(p1posX<35 || p1posY<35){ p1posY=35; p1posX=35; randno.setText("aman wonnn !!"); gB.setText("Start Again !"); break; }
 
         }
  }
@@ -175,7 +205,7 @@ public class MainGrid extends Application {
             if(poscirc2%2 == 0)p2posX -= 70;
             if(p2posX>665){ p2posY-=70; p2posX-=70; poscirc2 ++; }
             if(p2posX<35){ p2posY-=70; p2posX+=70; poscirc2 ++; }
-            if(p2posX<35 || p2posY<35){ p2posY=35; p2posX=35; randno.setText("raghav wonnn !!"); gB.setText("Start Again !"); }
+            if(p2posX<35 || p2posY<35){ p2posY=35; p2posX=35; randno.setText("Raghav wonnn !!"); gB.setText("Start Again !"); break; }
 
         }
     }
